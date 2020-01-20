d47b4ddb544cb6957bf63daafc920652ba81aba9
Victor <victorfesa@gmail.com>
2020-01-20 18:12:21 -0300

    doom bot

--- doom_bot/README.md
+++ doom_bot/README.md
@@ -1,6 +1,7 @@
 # WIP
 
 ## Doom_bot
+
 `Doom_bot` foi criado utilizando Python e a API pública do `Discord`
 
 Envia mensagens fixas, aleatórias ou áudios a partir de comandos definidos em arquivo `.yaml`
@@ -9,4 +10,4 @@ Envia mensagens fixas, aleatórias ou áudios a partir de comandos definidos em 
 
 É necessário um arquivo `.yaml` com comandos estabelecidos e `.env` com token válido
 
-Os arquivos `MP3` devem estar na mesma pasta do programa
+Os arquivos `MP3` devem estar na mesma pasta do programa e a library FFmpeg deve ser adicionada ao PATH
--- doom_bot/doom_bot.py
+++ doom_bot/doom_bot.py
@@ -3,6 +3,7 @@ import os
 import yaml
 import random
 
+from datetime import datetime
 from discord.ext import commands
 from discord.utils import get
 from dotenv import load_dotenv
@@ -11,22 +12,33 @@ from os import system
 load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
 BOT_PREFIX = "!"
-# content from .yaml to commands_dict
-with open("commands.yaml", encoding="utf8") as infile:
-    commands_dict = yaml.safe_load(infile)
 
+with open("example_yaml.yaml", encoding="utf8") as infile:
+    commands_dict = yaml.safe_load(infile)
 bot = commands.Bot(command_prefix=BOT_PREFIX)
-# pre-defined message
+
+
+@bot.command(pass_context=True)
+async def dice(ctx):
+    if ctx.author == bot.user:
+        return
+    await ctx.channel.send(f"Dice roll: {random.randint(1, 6)}")
+
+
 async def send_callback(ctx):
     if ctx.author == bot.user:
         return
     await ctx.channel.send(commands_dict[ctx.command.qualified_name]["text"])
-# random message within choices
+
+
 async def random_send_callback(ctx):
     if ctx.author == bot.user:
         return
-    await ctx.channel.send(random.choice(commands_dict[ctx.command.qualified_name]["choices"]))
-# plays mp3 file (same name as ctx)
+    await ctx.channel.send(
+        random.choice(commands_dict[ctx.command.qualified_name]["choices"])
+    )
+
+
 async def audio_callback(ctx):
     if ctx.message.author.voice == None:
         return
@@ -38,23 +50,29 @@ async def audio_callback(ctx):
         await voice.move_to(channel)
     else:
         voice = await channel.connect()
-        print(f"The bot has connected to {channel}\n")
+        print(
+            f"{datetime.now().strftime('%H:%M:%S')} The bot has connected to {channel} "
+            f"[requested by {ctx.author.name} ({ctx.author})]"
+        )
     voice = get(bot.voice_clients, guild=ctx.guild)
 
     voice.play(
-        discord.FFmpegPCMAudio(
-            commands_dict[ctx.command.qualified_name]["file"]),
-            after=lambda e: print("finished playing")
+        discord.FFmpegPCMAudio(commands_dict[ctx.command.qualified_name]["file"]),
+        after=lambda e: print(
+            f"{datetime.now().strftime('%H:%M:%S')} "
+            f"Finished playing !{ctx.command.qualified_name}"
+        ),
     )
     voice.source = discord.PCMVolumeTransformer(voice.source)
-    voice.source.volume = 0.3
+    voice.source.volume = 0.35
     while voice.is_playing() == True:
         continue
     if voice and voice.is_connected():
         await voice.disconnect()
-        print(f"The bot has left {channel}")
+        print(f"{datetime.now().strftime('%H:%M:%S')} The bot has left {channel}\n")
     if ctx.author == bot.user:
         return
+
 
 # build all commands from dict & add to bot
 for command_name in commands_dict:
@@ -66,18 +84,24 @@ for command_name in commands_dict:
         func = random_send_callback
     else:
         break
-
     c = commands.Command(
-            func,
-            name=command_name,
-            help=commands_dict[command_name]["help"]
+        func, name=command_name, help=commands_dict[command_name]["help"]
     )
     bot.add_command(c)
-
-# shows when the bot is connected to discord
+# display when the bot is connected to discord
 @bot.event
 async def on_ready():
-    print(f"{bot.user.name} has connected to Discord!")
+    print(
+        f"{datetime.now().strftime('%H:%M:%S')} {bot.user.name} has connected to Discord!"
+    )
+
+
+# ignore unrelated commands
+@bot.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.CommandNotFound):
+        return
+    raise error
 
 
 bot.run(TOKEN)
